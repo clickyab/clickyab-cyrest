83817b7a8bfcc43c7bd701bd0884a112c99a0643
fzerorubigd <fzerorubigd@gmail.com>
2016-12-14 18:16:45 +0330

    fix the wrong error style in validation

--- src/modules/user/controllers/forgot.go
+++ src/modules/user/controllers/forgot.go
@@ -9,29 +9,15 @@ import (
 
 	"common/assert"
 
-	"gopkg.in/go-playground/validator.v9"
-	"gopkg.in/labstack/echo.v3"
 	"modules/misc/trans"
+
+	"gopkg.in/labstack/echo.v3"
 )
 
+// @Validate {
+// }
 type forgotPayload struct {
 	Email string `json:"email" validate:"email"`
-	usr   *aaa.User
-}
-
-func (fp *forgotPayload) Validate(ctx echo.Context) error {
-	if err := validator.New().Struct(fp); err != nil {
-		return err
-	}
-
-	m := aaa.NewAaaManager()
-	u, err := m.FindUserByEmail(fp.Email)
-	if err != nil {
-		return trans.E("email not found")
-	}
-
-	fp.usr = u
-	return nil
 }
 
 // forgotPassword get email
@@ -45,8 +31,15 @@ func (fp *forgotPayload) Validate(ctx echo.Context) error {
 func (u *Controller) forgotPassword(ctx echo.Context) error {
 	pl := u.MustGetPayload(ctx).(*forgotPayload)
 	//generate key for email
+
+	m := aaa.NewAaaManager()
+	usr, err := m.FindUserByEmail(pl.Email)
+	if err != nil {
+		return u.BadResponse(ctx, trans.E("email not found"))
+	}
+
 	key := <-utils.ID
-	assert.Nil(aredis.StoreKey(key, pl.usr.Email, ucfg.Cfg.TokenTimeout))
+	assert.Nil(aredis.StoreKey(key, usr.Email, ucfg.Cfg.TokenTimeout))
 
 	sendEmailCodeGen()
 	return u.OKResponse(
