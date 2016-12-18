f818feae4ae362fc1d935250d0c713c647583391
masihtehrani <mahmud.tehrani@gmail.com>
2016-12-18 18:06:30 +0330

    create channel
    fix #519

--- Makefile
+++ Makefile
@@ -156,7 +156,7 @@ swagger-cleaner:
 swagger-client: tools-swagger
 	GOPATH=$(ROOT) cd $(ROOT)/src && $(BIN)/swagger generate client -f $(ROOT)/3rd/swagger/cyrest.yaml
 
-codegen: swagger-ui swagger-cleaner codegen-misc codegen-user codegen-category codegen-location
+codegen: swagger-ui swagger-cleaner codegen-misc codegen-user codegen-category codegen-location codegen-channel
 	@cp $(WORK_DIR)/swagger/out.yaml $(ROOT)/3rd/swagger/cyrest.yaml
 	@cp $(WORK_DIR)/swagger/out.json $(ROOT)/3rd/swagger/cyrest.json
 	@echo "Done"
--- src/modules/channel/chn/channel.go
+++ src/modules/channel/chn/channel.go
@@ -1,26 +1,58 @@
 // Package cat is the models for category module
 package chn
 
-import "time"
+import (
+	"common/assert"
+	"common/models/common"
+	"time"
+)
+
+//'pending', 'rejected','accepted','archive'
+const (
+	ChannelStatusPending  ChannelStatus = "pending"
+	ChannelStatusRejected ChannelStatus = "rejected"
+	ChannelStatusAccepted ChannelStatus = "accepted"
+	ChannelStatusArchive  ChannelStatus = "archive"
+)
+
+type (
+	// ChannelStatus is the channel status
+	// @Enum{
+	// }
+	ChannelStatus string
+)
 
 // Category model
 // @Model {
 //		table = channels
 //		primary = true, id
-//		find_by = id
+//		find_by = id,user_id
 //		list = yes
 // }
 type Channel struct {
-	ID        int64     `db:"id" json:"id"`
-	UserID    int64     `json:"user_id" db:"user_id"`
-	Name      string    `json:"name" db:"name"`
-	Link      string    `json:"link" db:"link"`
-	AdminID   int64     `json:"admin_id" db:"admin_id"`
-	Status    int       `json:"status" db:"status"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	ID        int64             `db:"id" json:"id"`
+	UserID    int64             `json:"user_id" db:"user_id"`
+	Name      string            `json:"name" db:"name"`
+	Link      common.NullString `json:"link" db:"link"`
+	Admin     common.NullString `json:"admin" db:"admin"`
+	Status    ChannelStatus     `json:"status" db:"status"`
+	CreatedAt time.Time         `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time         `db:"updated_at" json:"updated_at"`
 }
 
 func (c *Channel) Initialize() {
 
+}
+
+func (m *Manager) Create(admin, link, name string, status ChannelStatus, userID int64) *Channel {
+
+	ch := &Channel{
+		Admin:  common.NullString{Valid: admin != "", String: admin},
+		Link:   common.NullString{Valid: link != "", String: link},
+		Name:   name,
+		Status: status,
+		UserID: userID,
+	}
+	assert.Nil(m.CreateChannel(ch))
+	return ch
 }
