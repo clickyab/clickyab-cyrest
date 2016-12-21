06af53a1bbfde6f4a3ff9b04f93348524413c46d
slifer2015 <dara51php@gmail.com>
2016-12-21 18:03:05 +0330

    add campaign data-table

--- src/modules/campaign/cmp/campaign.go
+++ src/modules/campaign/cmp/campaign.go
@@ -4,7 +4,10 @@ package cmp
 import (
 	"common/assert"
 	"common/models/common"
+	"fmt"
+	"modules/misc/base"
 	"modules/user/aaa"
+	"strings"
 	"time"
 )
 
@@ -16,15 +19,14 @@ import (
 //		list = yes
 // }
 type Campaign struct {
-	ID     int64  `db:"id" json:"id"`
-	UserID int64  `json:"user_id" db:"user_id"`
-	Name   string `json:"name" db:"name"`
-	//Status    CampaignStatus `json:"status" db:"status"`
-	Active    CampaignActive  `json:"active" db:"active"`
-	Start     common.NullTime `json:"start" db:"start"`
-	End       common.NullTime `json:"end" db:"end"`
-	CreatedAt time.Time       `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time       `db:"updated_at" json:"updated_at"`
+	ID        int64           `db:"id" json:"id" sort:"true" title:"ID" map:"campaigns.id"`
+	UserID    int64           `json:"user_id" db:"user_id" title:"UserID"`
+	Name      string          `json:"name" db:"name" search:"true" title:"Name"`
+	Active    CampaignActive  `json:"active" db:"active" filter:"true" title:"Status"`
+	Start     common.NullTime `json:"start" db:"start" title:"Start"`
+	End       common.NullTime `json:"end" db:"end" title:"End"`
+	CreatedAt time.Time       `db:"created_at" json:"created_at" sort:"true" title:"Created at"`
+	UpdatedAt time.Time       `db:"updated_at" json:"updated_at" sort:"true" title:"Updated at"`
 }
 
 const (
@@ -44,6 +46,73 @@ type (
 	// }
 	CampaignActive string
 )
+
+//CampaignDataTable is the role full data in data table, after join with other field
+// @DataTable {
+//		url = /list
+//		entity = campaign
+//		view = campaign_list:self
+//		controller = modules/campaign/controllers
+//		fill = FillCampaignDataTableArray
+//		_edit = campaign_edit:self
+//		_change = campaign_manage:global
+// }
+type CampaignDataTable struct {
+	Campaign
+	Email    string `db:"email" json:"email" search:"true" title:"Email"`
+	ParentID int64  `db:"parent_id_dt" json:"parent_id" visible:"false"`
+	OwnerID  int64  `db:"owner_id_dt" json:"owner_id" visible:"false"`
+}
+
+// FillCampaignDataTableArray is the function to handle
+func (m *Manager) FillCampaignDataTableArray(u base.PermInterfaceComplete, filters map[string]string, search map[string]string, sort, order string, p, c int) (CampaignDataTableArray, int64) {
+	var params []interface{}
+	var res CampaignDataTableArray
+	var where []string
+
+	countQuery := fmt.Sprintf("SELECT COUNT(campaigns.id) FROM %s LEFT JOIN %s ON %s.id=%s.user_id", CampaignTableFull, aaa.UserTableFull, aaa.UserTableFull, CampaignTableFull)
+	query := fmt.Sprintf("SELECT campaigns.*,campaigns.user_id AS owner_id_dt,CASE WHEN users.parent_id IS NOT NULL THEN users.parent_id ELSE 0 END AS parent_id_dt,users.email FROM %s LEFT JOIN %s ON %s.id=%s.user_id", CampaignTableFull, aaa.UserTableFull, aaa.UserTableFull, CampaignTableFull)
+	for field, value := range filters {
+		where = append(where, fmt.Sprintf(CampaignTableFull+".%s=%s", field, "?"))
+		params = append(params, value)
+	}
+
+	for column, val := range search {
+		where = append(where, fmt.Sprintf("%s LIKE ?", column))
+		params = append(params, fmt.Sprintf("%s"+val+"%s", "%", "%"))
+	}
+
+	currentUserID := u.GetID()
+	highestScope := u.GetCurrentScope()
+
+	if highestScope == base.ScopeSelf {
+		where = append(where, fmt.Sprintf("%s.user_id=?", CampaignTableFull))
+		params = append(params, currentUserID)
+	} else if highestScope == base.ScopeParent {
+		where = append(where, "users.parent_id=?")
+		params = append(params, currentUserID)
+	}
+
+	//check for perm
+	if len(where) > 0 {
+		query += " WHERE "
+		countQuery += " WHERE "
+	}
+	query += strings.Join(where, " AND ")
+	countQuery += strings.Join(where, " AND ")
+	limit := c
+	offset := (p - 1) * c
+	if sort != "" {
+		query += fmt.Sprintf(" ORDER BY %s %s ", sort, order)
+	}
+	query += fmt.Sprintf(" LIMIT %d OFFSET %d ", limit, offset)
+	count, err := m.GetDbMap().SelectInt(countQuery, params...)
+	assert.Nil(err)
+
+	_, err = m.GetDbMap().Select(&res, query, params...)
+	assert.Nil(err)
+	return res, count
+}
 
 func (c *Campaign) Initialize() {
 
