4d52cd86a93d5ce9cb8328d0829dc5347e1175ee
mazafard <mazafard@gmail.com>
2016-12-28 09:31:34 +0330

    add model & some function

--- src/test/main.go
+++ src/test/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
-	"common/assert"
-	"common/tgo"
-	"net"
+	"log"
+	"gopkg.in/telegram-bot-api.v4"
 )
 
 func main() {
-	_, err := tgo.NewTelegramCli(net.IPv4(172, 17, 0, 1), 9010)
-assert.Nil(err)
+	//_, err := tgo.NewTelegramCli(net.IPv4(172, 17, 0, 1), 9010)
+	//assert.Nil(err)
 
+	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
