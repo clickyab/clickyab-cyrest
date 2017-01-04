167cc34b3bbb7223dad3953955841cccb6c24bd7
slifer2015 <dara51php@gmail.com>
2017-01-04 11:38:05 +0330

    implement ad create
    Fix #600

--- src/modules/ad/controllers/ad-crud.go
+++ src/modules/ad/controllers/ad-crud.go
@@ -2,18 +2,10 @@ package ad
 
 import (
 	"modules/ad/ads"
-	"modules/misc/trans"
-	"modules/user/aaa"
-	"net/http"
-	"strconv"
 
 	"modules/user/middlewares"
 
-	"common/models/common"
-
 	"common/assert"
-
-	"modules/misc/middlewares"
 
 	echo "gopkg.in/labstack/echo.v3"
 )
@@ -21,113 +13,12 @@ import (
 // @Validate {
 // }
 type AdPayload struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Link        string `json:"link"`
-}
-
-//	editAd
-//	@Route	{
-//	url	=	/:id
-//	method	= put
-//	payload	= AdPayload
-//	resource = edit_ad:self
-//	middleware = authz.Authenticate
-//	200 = ads.Ad
-//	400 = base.ErrorResponseSimple
-//	}
-func (u *Controller) editAd(ctx echo.Context) error {
-	pl := u.MustGetPayload(ctx).(*AdPayload)
-	id, err := strconv.ParseInt(ctx.Param("id"), 10, 0)
-	if err != nil {
-		return u.NotFoundResponse(ctx, nil)
-	}
-	m := ads.NewAdsManager()
-	ad, err := m.FindAdByID(id)
-	if err != nil {
-		return u.NotFoundResponse(ctx, nil)
-	}
-	owner, err := aaa.NewAaaManager().FindUserByID(ad.UserID)
-	if err != nil {
-		return u.NotFoundResponse(ctx, nil)
-	}
-	currentUser, ok := authz.GetUser(ctx)
-	if !ok {
-		return u.NotFoundResponse(ctx, nil)
-	}
-	_, b := currentUser.HasPermOn("edit_ad", owner.ID, owner.DBParentID.Int64)
-	if !b {
-		return ctx.JSON(http.StatusForbidden, trans.E("user can't access"))
-	}
-	ad.ID = id
-	ad.Link = common.NullString{Valid: pl.Link != "", String: pl.Link}
-	ad.Description = common.NullString{Valid: pl.Description != "", String: pl.Description}
-	ad.Name = pl.Name
-	ch := m.EditAd(owner, ad)
-	return u.OKResponse(ctx, ch)
-}
-
-// @Validate {
-// }
-type statusPayload struct {
-	Status ads.AdStatus `json:"status" validate:"required"`
-}
-
-// Validate custom validation for status
-func (lp *statusPayload) ValidateExtra(ctx echo.Context) error {
-	if !lp.Status.IsValid() {
-		return middlewares.GroupError{
-			"status": trans.E("is invalid"),
-		}
-	}
-	return nil
-}
-
-//	statusAd status ad
-//	@Route	{
-//	url	=	/status/:id
-//	method	= put
-//	payload	= statusPayload
-//	resource = status_ad:parent
-//	middleware = authz.Authenticate
-//	200 = ads.Ad
-//	400 = base.ErrorResponseSimple
-//	}
-func (u *Controller) statusAd(ctx echo.Context) error {
-	pl := u.MustGetPayload(ctx).(*statusPayload)
-	id, err := strconv.ParseInt(ctx.Param("id"), 10, 0)
-	if err != nil {
-		return u.NotFoundResponse(ctx, nil)
-	}
-	m := ads.NewAdsManager()
-	adds, err := m.FindAdByID(id)
-	if err != nil {
-		return u.NotFoundResponse(ctx, nil)
-	}
-	currentUser, ok := authz.GetUser(ctx)
-	if !ok {
-		return u.NotFoundResponse(ctx, nil)
-	}
-
-	owner, err := aaa.NewAaaManager().FindUserByID(adds.UserID)
-	if err != nil {
-		return u.NotFoundResponse(ctx, nil)
-	}
-	_, b := currentUser.HasPermOn("status_ad", owner.ID, owner.DBParentID.Int64)
-	if !b {
-		return ctx.JSON(http.StatusForbidden, trans.E("user can't access"))
-	}
-
-	adds.ID = id
-	adds.Status = pl.Status
-	assert.Nil(m.UpdateAd(adds))
-	return u.OKResponse(ctx, adds)
-
+	Name string `json:"name" validate:"required" error:"name is required"`
 }
 
 //	create create ad
 //	@Route	{
-//		url	=	/create
+//		url	=	/
 //		method	= post
 //		payload	= AdPayload
 //		resource = create_ad:self
@@ -137,9 +28,7 @@ func (u *Controller) statusAd(ctx echo.Context) error {
 //	}
 func (u *Controller) create(ctx echo.Context) error {
 	pl := u.MustGetPayload(ctx).(*AdPayload)
-
 	m := ads.NewAdsManager()
-
 	currentUser, ok := authz.GetUser(ctx)
 
 	if !ok {
@@ -147,12 +36,11 @@ func (u *Controller) create(ctx echo.Context) error {
 	}
 
 	newAd := &ads.Ad{
-		Link:        common.MakeNullString(pl.Link),
-		Description: common.MakeNullString(pl.Description),
-		Name:        pl.Name,
-		Status:      ads.AdStatusPending,
-		Type:        ads.AdTypeImg,
-		UserID:      currentUser.ID,
+		Name:            pl.Name,
+		AdArchiveStatus: ads.AdArchiveStatusNo,
+		AdPayStatus:     ads.AdPayStatusNo,
+		AdAdminStatus:   ads.AdAdminStatusPending,
+		UserID:          currentUser.ID,
 	}
 	assert.Nil(m.CreateAd(newAd))
 	return u.OKResponse(ctx, newAd)
--- src/modules/ad/migrations/20161224133932_ad.sql
+++ src/modules/ad/migrations/20161224133932_ad.sql
@@ -2,24 +2,38 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
 
+
+CREATE TABLE plans
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(60) NOT NULL,
+  description TEXT,
+  price INT(11),
+  view INT(11)
+
+);
+
 CREATE TABLE ads
 (
   id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_id INT(11) NOT NULL,
+  plan_id INT(11),
   name VARCHAR(60) NOT NULL,
-  type ENUM('img','document','video'),
-  media TEXT,
   description TEXT,
-  link TEXT,
-  status ENUM('pending', 'rejected','accepted','archive'),
+  src TEXT,
+  admin_status ENUM('pending', 'rejected','accepted'),
+  archive_status ENUM('yes', 'no'),
+  pay_status ENUM('yes', 'no'),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP DEFAULT '0000-00-00 00:00:00' NOT NULL,
-  CONSTRAINT ads_id_users_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
+  CONSTRAINT ads_id_users_id_fk FOREIGN KEY (user_id) REFERENCES users(id),
+  CONSTRAINT plans_id_plan_id_fk FOREIGN KEY (plan_id) REFERENCES plans(id)
 
 );
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
+DROP TABLE plans;
 DROP TABLE ads;
 
