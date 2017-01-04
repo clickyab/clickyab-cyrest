5c2f1a0243cb7574bc425a7f4f57304182fc40e5
mazafard <mazafard@gmail.com>
2017-01-04 16:23:51 +0330

    get all plans

---
+++ src/modules/plan/migrations/20170103195842_plan.sql
@@ -0,0 +1,19 @@
+
+-- +migrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+
+CREATE TABLE plans
+(
+    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL,
+    description  TEXT
+    active ENUM('yes', 'no'),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT '0000-00-00 00:00:00' NOT NULL,
+
+);
+
+-- +migrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
+
+drop TABLE plans
