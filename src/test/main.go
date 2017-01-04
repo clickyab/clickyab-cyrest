d4fa767adaaa11f4a903879958e5001e6de62bf1
fzerorubigd <fzerorubigd@gmail.com>
2017-01-04 16:13:38 +0330

    add tbot structure

--- src/test/main.go
+++ src/test/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"common/assert"
 	"common/config"
 	"common/initializer"
 
-	"common/rabbit"
-	"common/utils"
-	"modules/cyborg/commands"
+	"common/tgbot"
+
+	"gopkg.in/telegram-bot-api.v4"
 )
 
 func main() {
@@ -14,9 +15,14 @@ func main() {
 	config.InitApplication()
 
 	defer initializer.Initialize().Finalize()
-	j := commands.GetLastCommand{
-		HashKey: <-utils.ID,
-		Channel: "tst1234567",
-	}
-	rabbit.MustPublish(j)
+
+	b := tgbot.NewTelegramBot("273335144:AAEv4uPeo68X7Scc3MLKxwMO1YI3JFkWiJM")
+
+	b.RegisterMessageHandler("/test", func(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
+		msg := tgbotapi.NewMessage(m.Chat.ID, "<b>Hi</b> Dude! use /test_for_this")
+		msg.ParseMode = "HTML"
+		_, err := bot.Send(msg)
+		assert.Nil(err)
+	})
+	assert.Nil(b.Start())
 }
