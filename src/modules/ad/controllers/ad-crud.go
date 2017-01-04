da02078abe2b88aec891d644dcf47aa3c9efed08
slifer2015 <dara51php@gmail.com>
2017-01-04 14:05:46 +0330

    implement change archive status and admin status Fix #611

--- src/modules/ad/controllers/ad-crud.go
+++ src/modules/ad/controllers/ad-crud.go
@@ -3,17 +3,38 @@ package ad
 import (
 	"modules/ad/ads"
 
-	"modules/user/middlewares"
+	echo "gopkg.in/labstack/echo.v3"
 
 	"common/assert"
 
-	echo "gopkg.in/labstack/echo.v3"
+	"modules/misc/middlewares"
+	"modules/misc/trans"
+	"modules/user/aaa"
+	"modules/user/middlewares"
+	"net/http"
+	"strconv"
 )
 
 // @Validate {
 // }
 type AdPayload struct {
 	Name string `json:"name" validate:"required" error:"name is required"`
+}
+
+// @Validate {
+// }
+type AdAdminStatusPayload struct {
+	AdAdminStatus ads.AdAdminStatus `json:"admin_status" validate:"required" error:"status is required"`
+}
+
+// Validate custom validation for user scope
+func (lp *AdAdminStatusPayload) ValidateExtra(ctx echo.Context) error {
+	if !lp.AdAdminStatus.IsValid() {
+		return middlewares.GroupError{
+			"status": trans.E("status is invalid"),
+		}
+	}
+	return nil
 }
 
 //	create create ad
@@ -44,4 +65,77 @@ func (u *Controller) create(ctx echo.Context) error {
 	}
 	assert.Nil(m.CreateAd(newAd))
 	return u.OKResponse(ctx, newAd)
+}
+
+//	changeAdminStatus change admin status for ad
+//	@Route	{
+//		url	=	/change-admin/:id
+//		method	= put
+//		payload	= AdAdminStatusPayload
+//		resource = change_admin_ad:parent
+//		middleware = authz.Authenticate
+//		200 = ads.Ad
+//		400 = base.ErrorResponseSimple
+//	}
+func (u *Controller) changeAdminStatus(ctx echo.Context) error {
+	pl := u.MustGetPayload(ctx).(*AdAdminStatusPayload)
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 0)
+	if err != nil {
+		return u.NotFoundResponse(ctx, nil)
+	}
+	m := ads.NewAdsManager()
+	currentAd, err := m.FindAdByID(id)
+	if err != nil {
+		return u.NotFoundResponse(ctx, nil)
+	}
+	currentUser, ok := authz.GetUser(ctx)
+	if !ok {
+		return u.NotFoundResponse(ctx, nil)
+	}
+	owner, err := aaa.NewAaaManager().FindUserByID(currentAd.UserID)
+	_, b := currentUser.HasPermOn("change_admin_ad", owner.ID, owner.DBParentID.Int64)
+	if !b {
+		return ctx.JSON(http.StatusForbidden, trans.E("user can't access"))
+	}
+	currentAd.AdAdminStatus = pl.AdAdminStatus
+	assert.Nil(m.UpdateAd(currentAd))
+
+	return u.OKResponse(ctx, currentAd)
+}
+
+//	changeArchiveStatus change archive status for ad
+//	@Route	{
+//		url	=	/change-archive/:id
+//		method	= put
+//		resource = change_archive_ad:self
+//		middleware = authz.Authenticate
+//		200 = ads.Ad
+//		400 = base.ErrorResponseSimple
+//	}
+func (u *Controller) changeArchiveStatus(ctx echo.Context) error {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 0)
+	if err != nil {
+		return u.NotFoundResponse(ctx, nil)
+	}
+	m := ads.NewAdsManager()
+	currentAd, err := m.FindAdByID(id)
+	if err != nil {
+		return u.NotFoundResponse(ctx, nil)
+	}
+	currentUser, ok := authz.GetUser(ctx)
+	if !ok {
+		return u.NotFoundResponse(ctx, nil)
+	}
+	owner, err := aaa.NewAaaManager().FindUserByID(currentAd.UserID)
+	_, b := currentUser.HasPermOn("change_admin_ad", owner.ID, owner.DBParentID.Int64)
+	if !b {
+		return ctx.JSON(http.StatusForbidden, trans.E("user can't access"))
+	}
+	if currentAd.AdArchiveStatus==ads.AdArchiveStatusYes{
+		currentAd.AdArchiveStatus=ads.AdArchiveStatusNo
+	}else{
+		currentAd.AdArchiveStatus=ads.AdArchiveStatusYes
+	}
+	assert.Nil(m.UpdateAd(currentAd))
+	return u.OKResponse(ctx, currentAd)
 }
