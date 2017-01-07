d29bcf7d456595733628667348cb930fe976dcff
mazafard <mazafard@gmail.com>
2017-01-07 13:42:20 +0330

    delete unesed imposted library

--- src/modules/plan/controllers/plan-crud.go
+++ src/modules/plan/controllers/plan-crud.go
@@ -1,10 +1,3 @@
 package plan
 
-import (
-	"modules/channel/chn"
-	"modules/misc/middlewares"
-	"modules/misc/trans"
-
-	echo "gopkg.in/labstack/echo.v3"
-)
- //@todo add crud for controller
+//@todo add crud for controller
