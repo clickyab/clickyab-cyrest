54ee26a31a9b7bc16d472410d6530e033b00df54
slifer2015 <dara51php@gmail.com>
2017-01-20 16:38:50 +0330

    fix lint migration

--- src/modules/telegram/teleuser/bot/verify.go
+++ src/modules/telegram/teleuser/bot/verify.go
@@ -4,16 +4,16 @@ import (
 	"common/assert"
 	"common/initializer"
 	"common/models/common"
+	"common/redis"
 	"fmt"
-	"modules/telegram/ad/ads"
 	"modules/telegram/channel/chn"
 	"modules/telegram/common/tgbot"
 	"modules/telegram/teleuser/tlu"
 	"strconv"
 	"strings"
 	"time"
+
 	"gopkg.in/telegram-bot-api.v4"
-	"common/redis"
 )
 
 const htmlMode = "HTML"
@@ -58,26 +58,6 @@ func (bb *bot) verify(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
 	}
 }
 
-func (bb *bot) updateAD(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
-	result := strings.Replace(m.Text, "/updatead-", "", 1)
-	id, err := strconv.ParseInt(result, 0, 10)
-	if err == nil {
-		tgbot.RegisterUserHandler(m.Chat.ID, func(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
-			defer tgbot.UnRegisterUserHandler(m.Chat.ID)
-			botChatID := strconv.FormatInt(m.Chat.ID, 10)
-			botMsgID := strconv.Itoa(m.MessageID)
-			n := ads.NewAdsManager()
-			currentAd, err := n.FindAdByID(id)
-			assert.Nil(err)
-			currentAd.BotChatID = common.MakeNullString(botChatID)
-			currentAd.BotMessageID = common.MakeNullString(botMsgID)
-			assert.Nil(n.UpdateAd(currentAd))
-
-		}, time.Minute)
-	}
-
-}
-
 func (bb *bot) wantAD(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
 	//find channels
 	chnManger := chn.NewChnManager()
@@ -106,7 +86,6 @@ func (bb *bot) wantAD(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
 func (bb *bot) Initialize() {
 
 	tgbot.RegisterMessageHandler("/verify", bb.verify)
-	tgbot.RegisterMessageHandler("/updatead", bb.updateAD)
 	tgbot.RegisterMessageHandler("/ad", bb.wantAD)
 }
 
