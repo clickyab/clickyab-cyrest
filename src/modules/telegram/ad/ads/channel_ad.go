d551f2fee0373372abc10952a8d61ff6e76c75fe
masihtehrani <mahmud.tehrani@gmail.com>
2017-02-05 12:32:39 +0330

    advertiser dashboard datatable
    fix #859

--- src/modules/telegram/ad/ads/channel_ad.go
+++ src/modules/telegram/ad/ads/channel_ad.go
@@ -133,21 +133,17 @@ func (m *Manager) FindChannelAdByChannelIDActive(a int64) ([]ChannelAdD, error) 
 	res := []ChannelAdD{}
 	_, err := m.GetDbMap().Select(
 		&res,
-		fmt.Sprintf("SELECT %s.*,%s.cli_telegram_id AS cli_message_ad,"+
-			"%s.position AS ad_position,"+
-			"%s.view AS plan_view"+
-			" FROM %s INNER JOIN %s ON %s.id=%s.ad_id INNER JOIN %s ON %s.id=%s.plan_id WHERE channel_id=? AND active='yes' AND end IS NULL",
+		fmt.Sprintf("SELECT %[1]s.*,%[2]s.cli_telegram_id AS cli_message_ad,"+
+			"%[2]s.position AS ad_position,"+
+			"%[3]s.view AS plan_view"+
+			" FROM %[1]s INNER JOIN %[2]s ON %[2]s.id=%[1]s.ad_id "+
+			" INNER JOIN %[3]s ON %[3]s.id=%[2]s.plan_id "+
+			" WHERE channel_id=? "+
+			" AND active='yes' "+
+			" AND end IS NULL",
 			ChannelAdTableFull,
 			AdTableFull,
-			AdTableFull,
 			PlanTableFull,
-			ChannelAdTableFull,
-			AdTableFull,
-			AdTableFull,
-			ChannelAdTableFull,
-			PlanTableFull,
-			PlanTableFull,
-			AdTableFull,
 		),
 		a,
 	)
@@ -193,28 +189,15 @@ func (m *Manager) ChooseAd(channelID int64) ([]SelectAd, error) {
 	res := []SelectAd{}
 	_, err := m.GetDbMap().Select(
 		&res,
-		fmt.Sprintf("SELECT %s.*,sum(%s.possible_view) as possible_view,sum(%s.view) as viewed ,%s.view as view "+
-			"FROM %s "+
-			"LEFT JOIN %s ON %s.id = %s.plan_id "+
-			"INNER JOIN %s on %s.ad_id = %s.id "+
-			"WHERE %s.channel_id != ? "+
-			"GROUP BY %s.ad_id ",
+		fmt.Sprintf("SELECT %[1]s.*,sum(%[2]s.possible_view) as possible_view,sum(%[2]s.view) as viewed ,%[3]s.view as view "+
+			"FROM %[1]s "+
+			"LEFT JOIN %[3]s ON %[3]s.id = %[1]s.plan_id "+
+			"INNER JOIN %[2]s on %[2]s.ad_id = %[1]s.id "+
+			"WHERE %[2]s.channel_id != ? "+
+			"GROUP BY %[2]s.ad_id ",
 			AdTableFull,
-			ChannelAdTableFull,
 			ChannelAdTableFull,
 			PlanTableFull,
-
-			AdTableFull,
-
-			PlanTableFull,
-			PlanTableFull,
-			AdTableFull,
-
-			ChannelAdTableFull,
-			ChannelAdTableFull,
-			AdTableFull,
-			ChannelAdTableFull,
-			ChannelAdTableFull,
 		),
 		channelID,
 	)
@@ -224,121 +207,6 @@ func (m *Manager) ChooseAd(channelID int64) ([]SelectAd, error) {
 	}
 
 	return res, nil
-}
-
-//ReportAdDataTable is the ad full data in data table, after join with other field
-// @DataTable {
-//		url = /report
-//		entity = adReport
-//		view = report_ad:self
-//		controller = modules/telegram/ad/adControllers
-//		fill = FillAdReportDataTableArray
-//		_edit = ad_edit:self
-//		_change = ad_manage:global
-// }
-type ReportAdDataTable struct {
-	Ad
-	View         int64           `db:"view" json:"view" sort:"true" title:"View"`
-	Active       ActiveStatus    `db:"active" json:"active" title:"ActiveStatus" filter:"true"`
-	Start        common.NullTime `db:"start" json:"start" sort:"true" title:"Start"`
-	End          common.NullTime `db:"end" json:"end" sort:"true" title:"End"`
-	Warning      int64           `db:"warning" json:"warning" sort:"true" title:"Warning"`
-	PossibleView int64           `db:"possible_view" json:"possible_view" sort:"true" title:"PossibleView"`
-	Email        string          `db:"email" json:"email" search:"true" title:"Email"`
-	ParentID     int64           `db:"-" json:"parent_id" visible:"false"`
-	OwnerID      int64           `db:"-" json:"owner_id" visible:"false"`
-	Actions      string          `db:"-" json:"_actions" visible:"false"`
-}
-
-// FillAdReportDataTableArray is the function to handle
-func (m *Manager) FillAdReportDataTableArray(
-	u base.PermInterfaceComplete,
-	filters map[string]string,
-	search map[string]string,
-	sort, order string, p, c int) (ReportAdDataTableArray, int64) {
-	var params []interface{}
-	var res ReportAdDataTableArray
-	var where []string
-
-	countQuery := fmt.Sprintf("SELECT COUNT(%s.ad_id) FROM %s "+
-		"LEFT JOIN %s ON %s.id=%s.ad_id "+
-		"LEFT JOIN %s ON %s.id=%s.user_id "+
-		"GROUP BY %s.ad_id",
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-
-		AdTableFull,
-		AdTableFull,
-		ChannelAdTableFull,
-
-		aaa.UserTableFull,
-		aaa.UserTableFull,
-		AdTableFull,
-
-		ChannelAdTableFull,
-	)
-	query := fmt.Sprintf("SELECT %s.*,%s.view,%s.warning,%s.active,%s.start,%s.possible_view,%s.end,%s.email FROM %s "+
-		"LEFT JOIN %s ON %s.id=%s.ad_id "+
-		"LEFT JOIN %s ON %s.id=%s.user_id ",
-		AdTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-		aaa.UserTableFull,
-		ChannelAdTableFull,
-
-		AdTableFull,
-		AdTableFull,
-		ChannelAdTableFull,
-
-		aaa.UserTableFull,
-		aaa.UserTableFull,
-		AdTableFull,
-	)
-	for field, value := range filters {
-		where = append(where, fmt.Sprintf("%s.%s=?", ChannelAdTableFull, field))
-		params = append(params, value)
-	}
-
-	for column, val := range search {
-		where = append(where, fmt.Sprintf("%s LIKE ?", column))
-		params = append(params, "%"+val+"%")
-	}
-
-	currentUserID := u.GetID()
-	highestScope := u.GetCurrentScope()
-
-	if highestScope == base.ScopeSelf {
-		where = append(where, fmt.Sprintf("%s.user_id=?", ChannelAdTableFull))
-		params = append(params, currentUserID)
-	} else if highestScope == base.ScopeParent {
-		where = append(where, fmt.Sprintf("%s.parent_id=?", aaa.UserTableFull))
-		params = append(params, currentUserID)
-	}
-
-	//check for perm
-	if len(where) > 0 {
-		query += " WHERE "
-		countQuery += " WHERE "
-	}
-	query += strings.Join(where, " AND ")
-	countQuery += strings.Join(where, " AND ")
-	limit := c
-	offset := (p - 1) * c
-	query += fmt.Sprintf(" GROUP BY %s.ad_id ", ChannelAdTableFull)
-	if sort != "" {
-		query += fmt.Sprintf(" ORDER BY %s %s ", sort, order)
-	}
-	query += fmt.Sprintf(" LIMIT %d OFFSET %d ", limit, offset)
-	count, err := m.GetDbMap().SelectInt(countQuery, params...)
-	assert.Nil(err)
-
-	_, err = m.GetDbMap().Select(&res, query, params...)
-	assert.Nil(err)
-	return res, count
 }
 
 //ReportChannelDataTable is the ad full data in data table, after join with other field
@@ -375,43 +243,20 @@ func (m *Manager) FillChannelReportDataTableArray(
 	var res ReportChannelDataTableArray
 	var where []string
 
-	countQuery := fmt.Sprintf("SELECT COUNT(%s.channel_id) FROM %s "+
-		"LEFT JOIN %s ON %s.id=%s.channel_id "+
-		"LEFT JOIN %s ON %s.id=%s.user_id "+
-		"GROUP BY %s.channel_id",
+	countQuery := fmt.Sprintf("SELECT COUNT(%[1]s.channel_id) FROM %[1]s "+
+		"INNER JOIN %[2]s ON %[2]s.id=%[1]s.channel_id "+
+		"INNER JOIN %[3]s ON %[3]s.id=%[2]s.user_id "+
+		"GROUP BY %[1]s.channel_id",
 		ChannelAdTableFull,
-		ChannelAdTableFull,
-
 		ChannelTableFull,
-		ChannelTableFull,
-		ChannelAdTableFull,
-
 		aaa.UserTableFull,
-		aaa.UserTableFull,
-		ChannelTableFull,
-
-		ChannelAdTableFull,
 	)
-	query := fmt.Sprintf("SELECT %s.*,%s.view,%s.warning,%s.active,%s.start,%s.possible_view,%s.end,%s.email FROM %s "+
-		"LEFT JOIN %s ON %s.id=%s.channel_id "+
-		"LEFT JOIN %s ON %s.id=%s.user_id ",
+	query := fmt.Sprintf("SELECT %[1]s.*,%[2]s.view,%[2]s.warning,%[2]s.active,%[2]s.start,%[2]s.possible_view,%[2]s.end,%[3]s.email FROM %[2]s "+
+		"INNER JOIN %[1]s ON %[1]s.id=%[2]s.channel_id "+
+		"INNER JOIN %[3]s ON %[3]s.id=%[1]s.user_id ",
 		ChannelTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
-		ChannelAdTableFull,
 		ChannelAdTableFull,
 		aaa.UserTableFull,
-		ChannelAdTableFull,
-
-		ChannelTableFull,
-		ChannelTableFull,
-		ChannelAdTableFull,
-
-		aaa.UserTableFull,
-		aaa.UserTableFull,
-		ChannelTableFull,
 	)
 	for field, value := range filters {
 		where = append(where, fmt.Sprintf("%s.%s=?", ChannelAdTableFull, field))
