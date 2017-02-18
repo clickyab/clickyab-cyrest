677c38f0d8e4a158a5407f0a6dd80d7efd1deecd
fzerorubigd <fzerorubigd@gmail.com>
2017-02-18 14:56:45 +0330

    fix the cron check

--- src/modules/telegram/cyborg/worker/cron_workers.go
+++ src/modules/telegram/cyborg/worker/cron_workers.go
@@ -3,50 +3,53 @@ package worker
 import (
 	"common/assert"
 	"common/models/common"
-	"fmt"
+	"common/rabbit"
+	"modules/misc/trans"
 	"modules/telegram/ad/ads"
 	bot2 "modules/telegram/bot/worker"
+	"modules/telegram/config"
 	"time"
 )
 
 // CronReview cron review for finished ads
 func (mw *MultiWorker) cronReview() error {
 	m := ads.NewAdsManager()
-	activeIndividualAds, err := m.SelectIndividualActiveAd()
-	assert.Nil(err)
-	if len(activeIndividualAds) <= 0 {
-		return nil
-	}
+	m.UpdateIndividualViewCount()
 
-	for k := range activeIndividualAds {
-		activeIndividualAds[k].Ad.View.Int64 = activeIndividualAds[k].Viewed
-		activeIndividualAds[k].Ad.View.Valid = true
-		err = m.UpdateAd(&activeIndividualAds[k].Ad)
+	finishedAds := m.FinishedActiveAds()
+	// TODO : transaction
+	for key := range finishedAds {
+		finishedAds[key].AdActiveStatus = ads.AdActiveStatusNo
+		assert.Nil(m.UpdateAd(&finishedAds[key]))
+		channelAd, err := m.FindChannelAdActiveByAdID(finishedAds[key].ID, ads.ActiveStatusYes)
 		assert.Nil(err)
-	}
-	allActiveAds, err := m.SelectAdsPlan()
-	assert.Nil(err)
-	for key := range allActiveAds {
-		if allActiveAds[key].Viewed < allActiveAds[key].Ad.View.Int64 {
-			channelAd, err := m.FindChannelAdActiveByAdID(allActiveAds[key].Ad.ID, ads.ActiveStatusYes)
-			assert.Nil(err)
-			for c := range channelAd {
-				//todo send message to admin channel
-				channelAd[c].ChannelAd.Active = ads.ActiveStatusNo
-				channelAd[c].ChannelAd.End = common.MakeNullTime(time.Now())
-				assert.Nil(m.UpdateChannelAd(&channelAd[c].ChannelAd))
-				str := fmt.Sprintf("you have have <%d> in channel \n please remove ad from <%s> channel", channelAd[c].ChannelAd.View, channelAd[c].Channel.Name)
-				bot2.SendWarnAction(&bot2.SendWarn{
-					AdID:      channelAd[c].ChannelAd.AdID,
-					ChannelID: channelAd[c].ChannelAd.ChannelID,
-					Msg:       str,
-					ChatID:    channelAd[c].ChannelAd.BotChatID,
-				})
-
-			}
+		for c := range channelAd {
+			//todo send message to admin channel
+			channelAd[c].ChannelAd.Active = ads.ActiveStatusNo
+			channelAd[c].ChannelAd.End = common.MakeNullTime(time.Now())
+			assert.Nil(m.UpdateChannelAd(&channelAd[c].ChannelAd))
+			str := trans.T("you have have <%d> in channel \n please remove ad from <%s> channel", channelAd[c].ChannelAd.View, channelAd[c].Channel.Name)
+			rabbit.MustPublish(&bot2.SendWarn{
+				AdID:      channelAd[c].ChannelAd.AdID,
+				ChannelID: channelAd[c].ChannelAd.ChannelID,
+				Msg:       str.String(),
+				ChatID:    channelAd[c].ChannelAd.BotChatID,
+			})
 		}
 	}
 
-	return nil
+	for _, chad := range m.GetWarningLimited(tcfg.Cfg.Telegram.LimitCountWarning) {
+		chad.Active = ads.ActiveStatusNo
+		chad.End = common.MakeNullTime(time.Now())
+		assert.Nil(m.UpdateChannelAd(&chad))
+		str := trans.T("Sorry, but since there is a lot of error, please remove the ads from your channel")
+		rabbit.MustPublish(&bot2.SendWarn{
+			AdID:      chad.AdID,
+			ChannelID: chad.ChannelID,
+			Msg:       str.String(),
+			ChatID:    chad.BotChatID,
+		})
+	}
 
+	return nil
 }
