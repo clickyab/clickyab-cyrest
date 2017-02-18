ac729fb805756f99b0154b4bae7d1d52f331b9ec
fzerorubigd <fzerorubigd@gmail.com>
2017-02-18 12:43:15 +0330

    add reshot command

--- src/modules/telegram/bot/reshot.go
+++ src/modules/telegram/bot/reshot.go
@@ -2,31 +2,29 @@ package bot
 
 import (
 	"common/rabbit"
-	"fmt"
 	"modules/telegram/ad/ads"
 	"modules/telegram/cyborg/commands"
 	"modules/telegram/teleuser/tlu"
 	"modules/user/aaa"
 	"strconv"
+	"strings"
 
-	"github.com/Sirupsen/logrus"
 	"gopkg.in/telegram-bot-api.v4"
 )
 
-func (bb *bot) doneORReject(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
-
-	b := ads.NewAdsManager()
-	doneSlice := doneRejectReg.FindStringSubmatch(m.Text)
-	logrus.Warn(doneSlice, len(doneSlice))
-	if len(doneSlice) != 3 {
+func (bb *bot) reshot(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
+	parts := strings.Split(m.Text, "_")
+	if len(parts) != 2 {
 		send(bot, m.Chat.ID, "your command is <b>not valid1</b>")
 		return
 	}
-	channelID, err := strconv.ParseInt(doneSlice[2], 10, 0)
+
+	channelID, err := strconv.ParseInt(parts[1], 10, 0)
 	if err != nil {
 		send(bot, m.Chat.ID, "your command is <b>not valid2</b>")
 		return
 	}
+
 	tele := tlu.NewTluManager()
 	telegramUser, err := tele.FindTeleUserByBotChatID(m.Chat.ID)
 	if err != nil {
@@ -39,36 +37,22 @@ func (bb *bot) doneORReject(bot *tgbotapi.BotAPI, m *tgbotapi.Message) {
 		send(bot, m.Chat.ID, "your telegram user is not in our system \n please register!")
 		return
 	}
-
+	b := ads.NewAdsManager()
 	channel, err := b.FindChannelByUserIDChannelID(user.ID, channelID)
 	if err != nil {
 		send(bot, m.Chat.ID, "you are not owner this channel")
 		return
 	}
-
-	if doneSlice[1] == "done" {
-		channelAd, err := b.FindChannelAdActiveByChannelID(channel.ID, ads.ActiveStatusNo)
-		if err != nil || len(channelAd) == 0 {
-			send(bot, m.Chat.ID, "your command is <b>not valid</b>")
-			return
-		}
-
-		rabbit.MustPublish(
-			commands.DiscoverAd{
-				Channel: channelID,
-				ChatID:  m.Chat.ID,
-			},
-		)
-
-		return
-	}
-	//reject command
-	err = b.DeleteChannelAdByChannelID(channel.ID)
-	if err != nil {
+	channelAd, err := b.FindChannelAdActiveByChannelID(channel.ID, ads.ActiveStatusNo)
+	if err != nil || len(channelAd) == 0 {
 		send(bot, m.Chat.ID, "your command is <b>not valid</b>")
 		return
 	}
-	send(bot, m.Chat.ID, fmt.Sprintf("ads reject in <b>%s</b> channel", channel.Name))
-	return
 
+	rabbit.MustPublish(
+		commands.DiscoverAd{
+			Channel: channelID,
+			ChatID:  m.Chat.ID,
+		},
+	)
 }
