bbc14c0326d9170d2823f06eba37da6d3bffee9c
slifer2015 <dara51php@gmail.com>
2017-02-19 12:40:04 +0330

    get appropriate plan

--- src/modules/telegram/ad/planControllers/plan-crud.go
+++ src/modules/telegram/ad/planControllers/plan-crud.go
@@ -7,8 +7,19 @@ import (
 
 	"strconv"
 
+	"modules/misc/trans"
+	"modules/user/aaa"
+	"modules/user/middlewares"
+	"net/http"
+
 	"gopkg.in/labstack/echo.v3"
 )
+
+// @Validate {
+// }
+type planPayload struct {
+	AdID int64 `json:"ad_id" validate:"required" error:"ad id is required"`
+}
 
 //	allPlan get all plans
 //	@Route	{
@@ -39,6 +50,40 @@ func (u *Controller) allIndividualPlan(ctx echo.Context) error {
 	m := ads.NewAdsManager()
 	plans, err := m.GetAllIndividualActivePlans()
 	assert.Nil(err)
+	return u.OKResponse(ctx, plans)
+}
+
+//	getAllAppropriatePlan get all appropriate plans
+//	@Route	{
+//	url	=	/appropriate
+//	method	= get
+//	resource = get_plan:self
+//	middleware = authz.Authenticate
+//	200 = plans
+//	400 = base.ErrorResponseSimple
+//	}
+func (u *Controller) getAllAppropriatePlan(ctx echo.Context) error {
+	var plans []ads.Plan
+	pl := u.MustGetPayload(ctx).(*planPayload)
+	m := ads.NewAdsManager()
+	currentAd, err := m.FindAdByID(pl.AdID)
+	if err != nil {
+		return u.NotFoundResponse(ctx, nil)
+	}
+	currentUser := authz.MustGetUser(ctx)
+	owner, err := aaa.NewAaaManager().FindUserByID(currentAd.UserID)
+	assert.Nil(err)
+	_, b := currentUser.HasPermOn("get_plan", owner.ID, owner.DBParentID.Int64)
+	if !b {
+		return ctx.JSON(http.StatusForbidden, trans.E("user can't access"))
+	}
+	if currentAd.CliMessageID.Valid { //get forwarded plan
+		plans, err = m.GetAllPromotionActivePlans()
+		assert.Nil(err)
+	} else {
+		plans, err = m.GetAllIndividualActivePlans()
+		assert.Nil(err)
+	}
 	return u.OKResponse(ctx, plans)
 }
 
