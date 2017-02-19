19f3d5c47eddf730f889124e1739e745664defc5
fzerorubigd <fzerorubigd@gmail.com>
2017-02-19 16:15:01 +0330

    fix the query

--- src/modules/telegram/ad/migrations/20170219105804_fixchannelad.sql
+++ src/modules/telegram/ad/migrations/20170219105804_fixchannelad.sql
@@ -3,8 +3,8 @@
 -- SQL in section 'Up' is executed when this migration is applied
 ALTER TABLE channel_ad DROP COLUMN start;
 ALTER TABLE channel_ad DROP COLUMN end;
-ALTER TABLE channel_ad DROP ADD COLUMN start TIMESTAMP NULL NULL;
-ALTER TABLE channel_ad DROP ADD COLUMN end TIMESTAMP NULL NULL;
+ALTER TABLE channel_ad ADD COLUMN start TIMESTAMP NULL NULL;
+ALTER TABLE channel_ad ADD COLUMN end TIMESTAMP NULL NULL;
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
