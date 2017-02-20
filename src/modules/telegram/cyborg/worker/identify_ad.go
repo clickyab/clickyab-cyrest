ed8e46b23eede3d2b4034265c126876b0cd241fb
slifer2015 <dara51php@gmail.com>
2017-02-20 12:02:58 +0330

    debug for identify ad

--- src/modules/telegram/cyborg/worker/identify_ad.go
+++ src/modules/telegram/cyborg/worker/identify_ad.go
@@ -6,10 +6,13 @@ import (
 	"modules/telegram/ad/ads"
 	"modules/telegram/config"
 	"modules/telegram/cyborg/commands"
+
+	"github.com/Sirupsen/logrus"
 )
 
 func (mw *MultiWorker) identifyAD(in *commands.IdentifyAD) (bool, error) {
 	// first try to resolve the channel
+	logrus.Debug("identify ad .... worker")
 	m := ads.NewAdsManager()
 	ad, err := m.FindAdByID(in.AdID)
 	assert.Nil(err)
@@ -19,7 +22,8 @@ func (mw *MultiWorker) identifyAD(in *commands.IdentifyAD) (bool, error) {
 	_, err = mw.sendMessage(tcfg.Cfg.Telegram.BotID, fmt.Sprintf("/updatead-%d", in.AdID))
 	assert.Nil(err)
 	_, err = mw.fwdMessage(tcfg.Cfg.Telegram.BotID, ad.CliMessageID.String)
-
+	logrus.Debug("identify ad .... worker")
+	logrus.Debug("identify ad .... worker ad cli message id", ad.CliMessageID.String)
 	assert.Nil(err)
 	return false, nil
 }
