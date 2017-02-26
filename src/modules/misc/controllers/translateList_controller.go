2bd1f066ebd6ef461e9536c6b1815476c8141dd7
fzerorubigd <fzero@rubi.gd>
2017-02-26 18:13:45 +0330

    add translation data table , not a generated one

---
+++ src/modules/misc/controllers/translateList_controller.go
@@ -0,0 +1,118 @@
+package misc
+
+import (
+	"common/assert"
+	"common/utils"
+	"encoding/json"
+	"modules/misc/base"
+	"modules/misc/t9n"
+
+	"modules/misc/trans"
+
+	"gopkg.in/labstack/echo.v3"
+)
+
+type listTranslatelistResponse struct {
+	Total      int64                       `json:"total"`
+	Data       t9n.TranslateDataTableArray `json:"data"`
+	Page       int                         `json:"page"`
+	PerPage    int                         `json:"per_page"`
+	Definition base.Columns                `json:"definition"`
+}
+
+var listTranslatelistDefinition base.Columns
+
+// @Route {
+// 		url = /translate/:lang
+//		method = get
+//		resource = translate_list:global
+//		_lang_ = string, the language
+//		_def_ = bool, show definition in result?
+//		200 = listTranslatelistResponse
+// }
+func (u *Controller) listTranslatelist(ctx echo.Context) error {
+	m := t9n.NewT9nManager()
+	p, c := utils.GetPageAndCount(ctx.Request(), false)
+
+	filter := make(map[string]string)
+
+	search := make(map[string]string)
+	lang := ctx.Param("lang")
+	if lang != trans.PersianLang && lang != trans.EnglishLang {
+		return u.NotFoundResponse(ctx, trans.E("not valid language"))
+	}
+	sort := ""
+	order := "ASC"
+
+	//pc := base.NewPermInterfaceComplete(usr, usr.ID, "translate_list", "global")
+	dt, cnt := m.FillTranslateDataTableArray(lang, filter, search, sort, order, p, c)
+	res := listTranslatelistResponse{
+		Total:   cnt,
+		Data:    dt,
+		Page:    p,
+		PerPage: c,
+	}
+	if ctx.Request().URL.Query().Get("def") == "true" {
+		res.Definition = listTranslatelistDefinition
+	}
+	return u.OKResponse(
+		ctx,
+		res,
+	)
+}
+
+func init() {
+	tmp := []byte(` [
+		{
+			"data": "id",
+			"name": "ID",
+			"searchable": false,
+			"sortable": false,
+			"visible": true,
+			"filter": false,
+			"title": "ID",
+			"filter_valid_map": null
+		},
+		{
+			"data": "text",
+			"name": "Text",
+			"searchable": false,
+			"sortable": false,
+			"visible": true,
+			"filter": false,
+			"title": "Text",
+			"filter_valid_map": null
+		},
+		{
+			"data": "lang",
+			"name": "Lang",
+			"searchable": false,
+			"sortable": false,
+			"visible": true,
+			"filter": false,
+			"title": "Lang",
+			"filter_valid_map": null
+		},
+		{
+			"data": "translated",
+			"name": "Translated",
+			"searchable": false,
+			"sortable": false,
+			"visible": true,
+			"filter": false,
+			"title": "Translated",
+			"filter_valid_map": null
+		},
+		{
+			"data": "_actions",
+			"name": "Actions",
+			"searchable": false,
+			"sortable": false,
+			"visible": false,
+			"filter": false,
+			"title": "Actions",
+			"filter_valid_map": null
+		}
+	] `)
+	assert.Nil(json.Unmarshal(tmp, &listTranslatelistDefinition))
+}
--- src/modules/misc/t9n/translation.go
+++ src/modules/misc/t9n/translation.go
@@ -2,8 +2,10 @@ package t9n
 
 import (
 	"common/assert"
+	"common/models/common"
 	"database/sql"
 	"fmt"
+	"strings"
 	"sync"
 )
 
@@ -105,6 +107,15 @@ func (m *Manager) AddMissing(txt string) error {
 	return nil
 }
 
+// TranslateDataTable is data table version of translation
+type TranslateDataTable struct {
+	ID         int64             `db:"id" json:"id"`
+	Text       string            `db:"text" json:"text"`
+	Lang       common.NullString `db:"lang" json:"lang"`
+	Translated common.NullString `db:"translated" json:"translated"`
+	Actions    string            `db:"-" json:"_actions" visible:"false"`
+}
+
 // CreateOnDuplicateUpdateTranslations try to save a new Translations in database
 func (m *Manager) CreateOnDuplicateUpdateTranslations(t *Translations) error {
 	q := fmt.Sprintf(
@@ -118,4 +129,79 @@ func (m *Manager) CreateOnDuplicateUpdateTranslations(t *Translations) error {
 		t.Translated,
 	)
 	return err
+}
+
+// FillTranslateDataTableArray is the function to handle
+func (m *Manager) FillTranslateDataTableArray(
+	lang string,
+	filters map[string]string,
+	search map[string]string,
+	sort, order string,
+	p, c int) (TranslateDataTableArray, int64) {
+	var params []interface{}
+	var res TranslateDataTableArray
+	var where []string
+
+	countQuery := fmt.Sprintf("SELECT COUNT(%[1]s.id) FROM %[1]s  ", StringsTableFull)
+
+	query := fmt.Sprintf(
+		`SELECT s.id, s.text, t.translated, t.lang FROM %[1]s AS s LEFT JOIN %[2]s AS t ON s.id = t.string_id AND t.lang = "%s" `,
+		StringsTableFull,
+		TranslationsTableFull,
+		lang,
+	)
+
+	for field, value := range filters {
+		where = append(where, fmt.Sprintf("%s=?", field))
+		params = append(params, value)
+	}
+
+	for column, val := range search {
+		where = append(where, fmt.Sprintf("%s LIKE ?", column))
+		params = append(params, "%"+val+"%")
+	}
+	//check for perm
+	if len(where) > 0 {
+		query += " WHERE "
+		countQuery += " WHERE "
+	}
+	query += strings.Join(where, " AND ")
+	countQuery += strings.Join(where, " AND ")
+	limit := c
+	offset := (p - 1) * c
+	if sort != "" {
+		query += fmt.Sprintf(" ORDER BY %s %s ", sort, order)
+	}
+	query += fmt.Sprintf(" LIMIT %d OFFSET %d ", limit, offset)
+	count, err := m.GetDbMap().SelectInt(countQuery, params...)
+	assert.Nil(err)
+	fmt.Println(query)
+	_, err = m.GetDbMap().Select(&res, query, params...)
+	assert.Nil(err)
+	return res, count
+}
+
+type (
+	// TranslateDataTableArray is the array of translation struct
+	TranslateDataTableArray []TranslateDataTable
+)
+
+// Filter is for filtering base on permission
+func (tdt TranslateDataTable) Filter() TranslateDataTable {
+	res := TranslateDataTable{}
+
+	res.ID = tdt.ID
+
+	res.Text = tdt.Text
+
+	res.Lang = tdt.Lang
+
+	res.Translated = tdt.Translated
+
+	res.Actions = tdt.Actions
+
+	action := []string{}
+
+	res.Actions = strings.Join(action, ",")
+	return res
 }
