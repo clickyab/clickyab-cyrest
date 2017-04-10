9219aa84c451f1cbb0e24b4dee8e5ee83f03f027
fzerorubigd <fzerorubigd@gmail.com>
2017-04-10 14:56:39 +0430

    add pprof route

---
+++ src/modules/misc/controllers/pprof.go
@@ -0,0 +1,20 @@
+package misc
+
+import (
+	"runtime/pprof"
+
+	"gopkg.in/labstack/echo.v3"
+)
+
+// getVersion generate a panic for test
+// @Route {
+// 		url = /pprof
+//		method = get
+//      500 = base.ErrorResponseSimple
+//		200 = base.NormalResponse
+// }
+func (u *Controller) doPProf(ctx echo.Context) error {
+	pprof.Lookup("goroutine").WriteTo(ctx.Response(), 1)
+
+	return nil
+}
