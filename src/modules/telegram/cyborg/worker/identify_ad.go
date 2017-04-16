b0c0b772e901ad7157a425f2330b8ee0402e4302
slifer2015 <dara51php@gmail.com>
2017-04-16 13:50:12 +0430

    update ad

--- src/modules/telegram/cyborg/worker/identify_ad.go
+++ src/modules/telegram/cyborg/worker/identify_ad.go
@@ -23,10 +23,13 @@ func (mw *MultiWorker) identifyAD(in *commands.IdentifyAD) (bool, error) {
 	_, err = mw.fwdMessage(tcfg.Cfg.Telegram.BotID, ad.CliMessageID.String)
 	assert.Nil(err)
 	res, err := mw.getLastMessages(tcfg.Cfg.Telegram.BotID, 1, 0)
+	assert.Nil(err)
 	//update promote data
 	b, err := json.Marshal(res[0])
 	assert.Nil(err)
 	ad.PromoteData = common.MakeNullString(string(b))
+	assert.Nil(err)
+	err = ads.NewAdsManager().UpdateAd(ad)
 	assert.Nil(err)
 	return false, nil
 }
